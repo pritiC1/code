cc9566bd64391a2d9875971743dea55e833b167b
pritiC1 <pritichaugule44@gmail.com>
2024-11-26 09:37:28 +0530

    4.py

--- 4.py
+++ 4.py
@@ -1,8 +1,15 @@
-search=input("enter search element:")
-list=["cat","dog","horse"]
+def search_list(user_list,name):
+    index=0
+    for i in user_list:
+        if(i.upper()==name.upper()):
+            return index
+        index=index+1
+    return -1
 
-for i in list:
-    if i in list:
-        print(i)
-    else:
-        print(-1)
+
+
+values=input("Enter values:")
+user_list=values.split()
+name=input("Enter the name to be search:")
+answer=search_list(user_list,name)
+print(answer)
